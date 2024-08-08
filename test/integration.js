a8508fb2e3b9fd9f61c637bd0e94a5f118bc56d0
Black-Thunder <972581+Black-Thunder@users.noreply.github.com>
2024-08-08 21:12:36 +0200

    updated mocha config

--- test/integration.js
+++ test/integration.js
@@ -2,11 +2,4 @@ const path = require("path");
 const { tests } = require("@iobroker/testing");
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.integration(path.join(__dirname, ".."), {
-	//            ~~~~~~~~~~~~~~~~~~~~~~~~~
-	// This should be the adapter's root directory
-
-	// To test against a different version of JS-Controller, you can change the version or dist-tag here.
-	// Make sure to remove this setting when you're done testing.
-	controllerVersion: "latest", // or a specific version like "4.0.1"
-});
+tests.integration(path.join(__dirname, ".."));
